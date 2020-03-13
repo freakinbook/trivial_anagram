0990e72b5cf98f6af8c8e2d06574c4a3068e65e5
Dmitrii Zviagin <freemited@gmail.com>
2020-03-13 21:46:59 +0300

    Initial commit

---
+++ Anagram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s1 = "pukPukaPurejjJ";
+			string s2 = "pkpuukaPurejjJ";
+			Console.WriteLine(AreAnagrams(s1,s2)); 
+			Console.ReadKey();
+		}
+
+		static bool AreAnagrams(string s1, string s2)
+		{
+			s1 = s1.ToLower();
+			s2 = s2.ToLower();
+			if (s1.Length != s2.Length)
+			{
+				return false;
+			}
+			for (int i = 0; i < s1.Length; i++)
+			{
+				if (s2.Contains(s1[i]))
+				{
+					s2 = s2.Remove(s2.IndexOf(s1[i]), 1);
+					continue;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return true;
+
+		}
+	}
+}
